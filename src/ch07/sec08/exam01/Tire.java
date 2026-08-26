1af185f3e9f653765781e94d7e29564f3d00aa84
dlwogmlzz <lwwljh90@gmail.com>
2026-08-26 09:15:19 +0900

    20260825_JavaStudy_다형성

---
+++ src/ch07/sec08/exam01/Tire.java
@@ -0,0 +1,8 @@
+package ch07.sec08.exam01;
+
+public class Tire {
+    // 메서드 선언
+    public void roll() {
+        System.out.println("일반 타이어가 회전합니다.");
+    }
+}
---
+++ src/ch07/sec08/exam02/DriverExample.java
@@ -0,0 +1,18 @@
+package ch07.sec08.exam02;
+
+public class DriverExample {
+    public static void main(String[] args) {
+        // Driver객체 생성
+        Driver driver = new Driver();
+
+        // 매개값으로 Bus 객체를 제공하고 driver() 메서드 호출
+        // driver.drive(new Bus())와 동일
+        Bus bus = new Bus();
+        driver.drive(bus);
+
+        // 매개값으로 Texi 객체를 제공하고 driver() 메서드 호출
+        // driver.drive(new Taxi())와 동일
+        Taxi taxi = new Taxi();
+        driver.drive(taxi);
+    }
+}
