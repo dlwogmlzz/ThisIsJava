071ec8e4f66b9a6c0d202873838f5051de42a79e
dlwogmlzz <lwwljh90@gmail.com>
2026-08-28 10:39:50 +0900

    20260828_JavaStudy #oop Getter / Setter

--- src/Example/No22_GetterSetter/Car.java
+++ src/Example/No22_GetterSetter/Car.java
@@ -1,6 +1,6 @@
-package Example;
+package Example.No22_GetterSetter;
 
-public class No22_GetterSetter {
+public class Car {
     // 필드
     private int speed;
     private boolean stop;
@@ -8,5 +8,28 @@ public class No22_GetterSetter {
     // 생성자
 
     // 메서드
-    
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        // this.speed = speed;
+        // - 일경우의 처리
+        if (speed < 0) {
+            this.speed = 0;
+            return;
+        } else {
+            this.speed = speed;
+        }
+    }
+
+    public boolean isStop() {
+        return stop;
+    }
+
+    public void setStop(boolean stop) {
+        this.stop = stop;
+        this.speed = 0;
+    }
 }
